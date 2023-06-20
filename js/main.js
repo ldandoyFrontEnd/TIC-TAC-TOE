d91b671d9c0b3cf7362fae22603f1111025815ed
ldandoyFrontEnd <ldandoy@gmail.com>
2023-06-20 10:31:39 +0200

    ajout de la récupération des élements en js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log('TIC TAC TOE v1')
+console.log('TIC TAC TOE v1');
+
+let player1El = document.querySelector('#player1');
+let player2El = document.querySelector('#player2');
+
+let player1ItemEl = document.querySelector('#player1 .item');
+let player1NameEl = document.querySelector('#player1 .name');
+let player1ScoreEl = document.querySelector('#player1 .score');
+
+let player2ItemEl = document.querySelector('#player2 .item');
+let player2NameEl = document.querySelector('#player2 .name');
+let player2ScoreEl = document.querySelector('#player2 .score');
+
+let msgEl = document.querySelector('#msg');
+let gridEl = document.querySelector('.grid');
